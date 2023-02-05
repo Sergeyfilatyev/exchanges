1f9b02d082395f406c26220927d2417a2b461e36
AndreyGOIT <ae.borga@gmail.com>
2023-02-05 16:55:15 +0200

    currentRates

--- src/pages/Rates.jsx
+++ src/pages/Rates.jsx
@@ -1,3 +1,31 @@
+import { fetchCurrentRates } from 'utils/fetchCurrentRates';
+import { useEffect } from 'react';
+import { useState } from 'react';
+
 export const Rates = () => {
-  return <h1>Rates</h1>;
+  const [rates, setRates] = useState(null);
+
+  useEffect(() => {
+    fetchCurrentRates().then(data => {
+      setRates(data.rates);
+    });
+  }, []);
+
+  return (
+    <>
+      <h1>Rates</h1>
+      <p>One USD is</p>
+      {rates && (
+        <ul>
+          {Object.entries(rates).map(([key, value]) => {
+            return (
+              <li key={key}>
+                {key}: {(1 / value).toFixed(2)}
+              </li>
+            );
+          })}
+        </ul>
+      )}
+    </>
+  );
 };
